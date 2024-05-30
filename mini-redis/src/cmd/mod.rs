35302ab399a3f33aacf18ac78239689a58454c0d
zhunaiwei <zhunaiwei@bytedance.com>
2024-05-30 20:28:24 +0800

    feat: add server subscribe

--- mini-redis/src/cmd/mod.rs
+++ mini-redis/src/cmd/mod.rs
@@ -40,6 +40,18 @@ impl Command {
         parse.finish()?;
         Ok(command)
     }
+
+    pub fn get_name(&self) -> &str {
+        match self {
+            Command::Get(_) => "get",
+            Command::Set(_) => "set",
+            Command::Publish(_) => "publish",
+            Command::Subscribe(_) => "subscribe",
+            Command::Unsubscribe(_) => "unsubscribe",
+            Command::Ping(_) => "ping",
+            Command::Unknown(cmd) => cmd.get_name(),
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -199,34 +211,34 @@ impl Subscribe {
     }
 
     pub async fn apply(mut self, db: &Db, connection: &mut Connection) -> crate::Result<()> {
-        let f = Frame::Simple("test".to_string());
-        connection.write_frame(&f).await?;
-        Ok(())
-        // let mut subscription = StreamMap::new();
-        // loop {
-        //     for channel_name in self.channels.drain(..) {
-        //         subscribe_to_channel(channel_name, &mut subscription, db, connection).await?;
-        //     }
-        //     select! {
-        //         Some((channel_name, msg)) = subscription.next() {
-        //             return make_message_frame(channel_name, msg);
-        //         }
-        //         res = connection.read_frame() => {
-        //             let frame = match res? {
-        //                 Some(frame) => frame,
-        //                 // This happens if the remote client has disconnected.
-        //                 None => return Ok(())
-        //             };
-        //
-        //             handle_command(
-        //                 frame,
-        //                 &mut self.channels,
-        //                 &mut subscriptions,
-        //                 dst,
-        //             ).await?;
-        //         }
-        //     }
-        // }
+        // let f = Frame::Simple("test".to_string());
+        // connection.write_frame(&f).await?;
+        // Ok(())
+        let mut subscriptions = StreamMap::new();
+        loop {
+            for channel_name in self.channels.drain(..) {
+                subscribe_to_channel(channel_name, &mut subscriptions, db, connection).await?;
+            }
+            select! {
+                Some((channel_name, msg)) = subscriptions.next() => {
+                   connection.write_frame(&make_message_frame(channel_name, msg)).await?;
+                }
+                res = connection.read_frame() => {
+                    let frame = match res? {
+                        Some(frame) => frame,
+                        // This happens if the remote client has disconnected.
+                        None => return Ok(())
+                    };
+
+                    handle_command(
+                        frame,
+                        &mut self.channels,
+                        &mut subscriptions,
+                        connection,
+                    ).await?;
+                }
+            }
+        }
     }
 }
 
@@ -258,12 +270,50 @@ fn make_subscribe_frame(channel_name: String, num_subs: usize) -> Frame {
     response
 }
 
+fn make_unsubscribe_frame(channel_name: String, num_subs: usize) -> Frame {
+    let mut response = Frame::array();
+    response.push_bulk(Bytes::from_static(b"unsubscribe"));
+    response.push_bulk(Bytes::from(channel_name));
+    response.push_int(num_subs as u64);
+    response
+}
+
 fn make_message_frame(channel_name: String, msg: Bytes) -> Frame {
     let mut response = Frame::array();
     response.push_bulk(Bytes::from_static(b"message"));
     response.push_bulk(Bytes::from(channel_name));
     response.push_bulk(msg);
     response
+}
+
+async fn handle_command(
+    frame: Frame,
+    subscribe_to: &mut Vec<String>,
+    subscriptions: &mut StreamMap<String, Messages>,
+    connection: &mut Connection,
+) -> crate::Result<()> {
+    match Command::from_frame(frame)? {
+        Command::Subscribe(subscribe) => {
+            subscribe_to.extend(subscribe.channels.into_iter());
+        }
+        Command::Unsubscribe(mut unsubscribe) => {
+            if unsubscribe.channels.is_empty() {
+                if unsubscribe.channels.is_empty() {
+                    unsubscribe.channels = subscriptions.keys().map(|channel_name| channel_name.to_string()).collect();
+                }
+                for channel_name in unsubscribe.channels {
+                    subscriptions.remove(&channel_name);
+                    let response = make_unsubscribe_frame(channel_name, subscriptions.len());
+                    connection.write_frame(&response).await?;
+                }
+            }
+        }
+        command => {
+            let cmd = Unknown::new(command.get_name());
+            cmd.apply(connection).await?;
+        }
+    }
+    Ok(())
 }
 
 #[derive(Clone, Debug)]
@@ -351,5 +401,12 @@ impl Unknown {
 
     pub fn get_name(&self) -> &str {
         &self.command_name
+    }
+
+    pub async fn apply(self, connection: &mut Connection) -> crate::Result<()> {
+        let frame = Frame::Error(format!("ERR unknown command {}", self.command_name));
+        debug!(?frame);
+        connection.write_frame(&frame).await?;
+        Ok(())
     }
 }
